bca5781587b3b9ba09d3a5f24e09833d147f65d4
ftabert <ftabert@sqli.com>
2018-10-09 16:43:53 +0200

    correction

---
+++ precache-manifest.274dcf4879254b8e201e2ceffd037e30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aa765fc90ec2c57e2184",
+    "url": "/static/js/main.aa765fc9.chunk.js"
+  },
+  {
+    "revision": "96b02c54617cdbb486ce",
+    "url": "/static/js/1.96b02c54.chunk.js"
+  },
+  {
+    "revision": "aa765fc90ec2c57e2184",
+    "url": "/static/css/main.46a5f3c1.chunk.css"
+  },
+  {
+    "revision": "6eb8d0bafe9e0fb900088be590a09d2a",
+    "url": "/index.html"
+  }
+];
